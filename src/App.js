3e5508c85b49712dd58882be550da1617ce96892
j-breedlove <jbreedlove@jasonbreedlove.dev>
2023-08-26 13:01:51 -0700

    category menu and category item components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,10 @@
+import CategoryItem from "./components/category-item/category-item.component";
+import CategoryMenu from "./components/category-menu/category-menu.component";
 function App() {
-  const categories = [
-    {
-      id: "1",
-      title: "Hats",
-    },
-    {
-      id: "2",
-      title: "Jackets",
-    },
-    {
-      id: "3",
-      title: "Sneakers",
-    },
-    {
-      id: "4",
-      title: "Women",
-    },
-    {
-      id: "5",
-      title: "Men",
-    },
-  ];
-
   return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <div className="category-container">
-          <div className="background-image" />
-          <div className="category-body-container">
-            <h2>{category.title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
+    <div>
+      {/*<CategoryItem />*/}
+      <CategoryMenu />
     </div>
   );
 }
