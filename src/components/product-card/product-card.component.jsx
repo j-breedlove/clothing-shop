b7244f7808d4b57439994f8323d42820be3688b3
j-breedlove <jbreedlove@jasonbreedlove.dev>
2023-08-29 15:59:27 -0700

    styling shop page

---
+++ src/components/product-card/product-card.component.jsx
@@ -0,0 +1,21 @@
+import "./product-card.styles.scss";
+import Button from "../button/button.component";
+import React from "react";
+
+const ProductCard = ({ product }) => {
+  const { name, price, imageUrl } = product;
+  return (
+    <div className={"product-card-container"}>
+      <div className="image-wrapper">
+        <img src={imageUrl} alt={name} />
+        <Button buttonType={"inverted"}>Add to cart</Button>
+      </div>
+      <div className={"footer"}>
+        <span className={"name"}>{name}</span>
+        <span className={"price"}>{price}</span>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
