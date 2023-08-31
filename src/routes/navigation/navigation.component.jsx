9d56b60aca02d0e57e97d75fa3cb83873db8715d
j-breedlove <jbreedlove@jasonbreedlove.dev>
2023-08-31 15:24:30 -0700

    add cart icon and toggle to open and close cart

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,25 @@
+// 1. Node Modules
 import React, { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
+
+// 2. Components
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+
+// 3. Assets & Styles
 import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
 import "./navigation.styles.scss";
+
+// 4. Contexts & Hooks
+import { CartContext } from "../../contexts/cart.context";
 import { UserContext } from "../../contexts/user.context";
+
+// 5. Utility functions & Constants
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   // console.log(currentUser);
   return (
@@ -28,7 +41,9 @@ const Navigation = () => {
               <span className={"nav-link"}>Sign In</span>
             </Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
