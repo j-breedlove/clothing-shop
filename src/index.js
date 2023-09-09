2a97c10176e3ec143b6d0349de1b8b31a5958758
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-09-09 13:47:27 -0700

    fix Index.js error regarding import of App.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-
-import App from "./App";
 import { UserProvider } from "./contexts/user.context";
 import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
