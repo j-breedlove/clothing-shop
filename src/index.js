d625ee4d4b5f7748c1227a30279e3db306acbc70
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-09-09 12:01:36 -0700

    remove category-menu and fix transient imageUrl prop

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App';
-import { UserProvider } from './contexts/user.context';
-import { CategoriesProvider } from './contexts/categories.context';
-import { CartProvider } from './contexts/cart.context';
+import App from "./App";
+import { UserProvider } from "./contexts/user.context";
+import { CategoriesProvider } from "./contexts/categories.context";
+import { CartProvider } from "./contexts/cart.context";
 
-import './index.scss';
+import "./index.scss";
 
-const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-render(
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
@@ -23,5 +23,4 @@ render(
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
-  rootElement
 );
